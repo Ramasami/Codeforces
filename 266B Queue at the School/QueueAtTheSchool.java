8392535858d5f93e4233c537926298b4db0aef13
Ramasami <ramasami.official@gmail.com>
2021-12-25 23:25:12 +0530

    266B

---
+++ 266B Queue at the School/QueueAtTheSchool.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class QueueAtTheSchool {
+    public static void main(String[] args) {
+        int n, t;
+        String s;
+        try (Scanner sc = new Scanner(System.in)) {
+            n = sc.nextInt();
+            t = sc.nextInt();
+            sc.nextLine();
+            s = sc.nextLine();
+            int[] line = new int[s.length()];
+            for (int i = 0; i < s.length(); i++)
+                line[i] = s.charAt(i) == 'B' ? 1 : 0;
+            while (t-- > 0) {
+                for (int j = 0; j < line.length - 1; j++) {
+                    if(line[j] == 1 && line[j+1] == 0) {
+                        line[j] = 0;
+                        line[j+1] = 1;
+                        j++;
+                    }
+                }
+            }
+            Arrays.stream(line).mapToObj(person -> (person == 1?"B":"G")).forEach(System.out::print);
+        }
+    }
+}
