f178c1bfaa5f6086bfc1aaed026a315890145a25
Ramasami <ramasami.official@gmail.com>
2021-12-25 23:51:18 +0530

    112A

---
+++ 112A Lucky Division/LuckyDivision.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if ((n % 4) * (n % 7) * (n % 44) * (n % 47) * (n % 74) * (n % 77) * (n % 444) * (n % 447) * (n % 474) * (n % 477) * (n % 744) * (n % 747) * (n % 774) * (n % 777) == 0)
+        cout << "YES";
+    else
+        cout << "NO";
+}
