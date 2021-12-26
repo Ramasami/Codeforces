0b0e07dafaa2978c6dd23897d200ed00523daa9e
Ramasami <ramasami.official@gmail.com>
2021-12-26 06:37:30 +0530

    1335A

---
+++ 1335A Candies and Two Sisters/CandiesAndTwoSisters.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long n;
+        cin >> n;
+        n -= (n + 1) % 2;
+        cout << n / 2;
+    }
+    return 0;
+}
