597b4f0a55ea44a853a021af8b1dd3e69944c093
Ramasami <ramasami.official@gmail.com>
2021-12-30 22:58:23 +0530

    1472A

---
+++ 1472A Cards for Friends/CardsForFriends.py
@@ -0,0 +1,20 @@
+def main():
+    t = int(input())
+    while(t > 0):
+        t -= 1
+        w, h, n = [int(x) for x in input().split()]
+        two = 1
+        while w % 2 == 0:
+            w /= 2
+            two *=2
+        while h % 2 == 0:
+            h /= 2
+            two *= 2
+        if two >= n:
+            print("YES")
+        else:
+            print("NO")
+
+
+if __name__ == "__main__":
+    main()
