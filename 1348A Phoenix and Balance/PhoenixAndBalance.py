50acd977c4425db3ce3954e7b93281a777ebc269
Ramasami <ramasami.official@gmail.com>
2021-12-30 21:27:00 +0530

    1348A

---
+++ 1348A Phoenix and Balance/PhoenixAndBalance.py
@@ -0,0 +1,12 @@
+def main():
+    t = int(input())
+    while(t > 0):
+        t -= 1
+        n = int(input())
+        print(int(2 * (2**(n/2) - 1)))
+
+
+if __name__ == "__main__":
+    main()
+
+# 2 4 8 16 32 64 128 256
