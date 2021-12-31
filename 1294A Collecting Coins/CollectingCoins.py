5b6a18b3236d8787247de662ed67cb20e33c6a3a
Ramasami <ramasami.official@gmail.com>
2021-12-31 19:45:31 +0530

    1294A

--- 1294A Collecting Coins/CollectingCoins.py
+++ 1294A Collecting Coins/CollectingCoins.py
@@ -3,7 +3,14 @@ def main():
     while(t > 0):
         t -= 1
         a, b, c, n = [int(x) for x in input().split()]
-        print("YES" if ((a+b+c+n) % 3) == 0 else "NO")
+        if (a+b+c+n)%3!=0:
+            print("NO")
+        else:
+            med = (a+b+c+n)/3
+            if med >= a and med >=b and med >=c:
+                print("YES")
+            else:
+                print("NO")
 
 
 if __name__ == "__main__":
