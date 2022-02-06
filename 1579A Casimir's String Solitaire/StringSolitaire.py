1f223095c3aec49a76ea888cbe4eca365feffd09
Ramasami <ramasami.official@gmail.com>
2022-02-07 01:29:01 +0530

    1579A

---
+++ 1579A Casimir's String Solitaire/StringSolitaire.py
@@ -0,0 +1,20 @@
+def main():
+    t = int(input())
+    while(t > 0):
+        t -= 1
+        s = input()
+        c = 0
+        for i in s:
+            if i == 'B':
+                c+=1
+            else:
+                c-=1
+        if c == 0:
+            print("YES")
+        else:
+            print("NO")
+
+
+
+if __name__ == "__main__":
+    main()
