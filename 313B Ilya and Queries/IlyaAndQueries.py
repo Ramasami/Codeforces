9c7f2f2003d963f0f67a6f3db7822c5444a8424d
Ramasami <ramasami.official@gmail.com>
2022-02-07 00:49:38 +0530

    313B

--- 313B Ilya and Queries/IlyaAndQueries.py
+++ 313B Ilya and Queries/IlyaAndQueries.py
@@ -1,17 +1,25 @@
 def main():
     s = input()
-    c = []
-    count = 0
-    c.append(0)
-    for i in range(1, len(s)):
-        if s[i] == s[i-1]:
-            count += 1
-        c.append(count)
+    m = len(s)
+    M = 0
+    queries = []
     t = int(input())
     while(t > 0):
         t -= 1
         l, r = [int(x) for x in input().split()]
-        print(c[r-1] - c[l-1])
+        m = min(m, l-1)
+        M = max(M, r - 1)
+        queries.append((l-1, r-1))
+    c = [0] * (M-m+1)
+    count = 0
+    c.append(0)
+    for i in range(m, M + 1):
+        if s[i] == s[i-1]:
+            count += 1
+        c[i-m] = count
+    for query in queries:
+        l, r = query
+        print(c[r - m] - c[l - m])
 
 
 if __name__ == "__main__":
